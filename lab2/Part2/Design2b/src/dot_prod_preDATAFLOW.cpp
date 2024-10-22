4d4e2722f0bd00398c1869712d0b2584f502b62d
Cloud User <centos@ip-172-31-83-237.ec2.internal>
2024-10-22 01:28:40 +0000

    Redid Design 2b

--- lab2/Part2/Design2b/src/dot_prod_preDATAFLOW.cpp
+++ lab2/Part2/Design2b/src/dot_prod_preDATAFLOW.cpp
@@ -10,35 +10,12 @@ const unsigned int VEC_LEN = 1011;
 const unsigned int v_len = VEC_LEN;
 
 extern "C" {
+void dot_prod(const int *vec_a,     // Read-Only Vector 1
+          const int *vec_b,         // Read-Only Vector 2
+          int *results,             // Output Result
+          const unsigned int vector_len //Vector size
+) {
 
-
-void read_vec(unsigned int *va_buffer, unsigned int *vb_buffer, 
-             const int *vec_a, const int *vec_b, int counter) {
-    #pragma HLS DATAFLOW
-    for(int i = 0; i < BLOCK_SIZE; i++) {
-        if (counter * BLOCK_SIZE + i >= VEC_LEN) {
-            va_buffer[i] = 0;
-            vb_buffer[i] = 0;
-        }
-        else {
-            va_buffer[i] = vec_a[counter * BLOCK_SIZE + i];
-            vb_buffer[i] = vec_b[counter * BLOCK_SIZE + i];
-        }
-    }
-}
-
-void vec_mul(unsigned int *va_buffer, unsigned int *vb_buffer, 
-             unsigned int *vout_buffer) {
-    #pragma HLS DATAFLOW
-    for(int i = 0; i < BLOCK_SIZE; i++) {
-        vout_buffer[i] = va_buffer[i] * vb_buffer[i];
-    }
-}
-
-void dot_prod(const int *vec_a,                 // Read-Only Vector 1
-              const int *vec_b,                 // Read-Only Vector 2
-              int *results,                     // Output Result
-              const unsigned int vector_len ) { //Vector size
 //TODO (2): Determine the correct depth
 #pragma HLS INTERFACE m_axi port = vec_a offset = slave depth = 1011 bundle = gmem
 #pragma HLS INTERFACE m_axi port = vec_b offset = slave depth = 1011 bundle = gmem
@@ -59,14 +36,26 @@ void dot_prod(const int *vec_a,                 // Read-Only Vector 1
     results[0] = 0;
 
     #pragma HLS PIPELINE off
-     for(int counter = 0; counter < NUM_BLOCK; counter++) {
-        read_vec(va_buffer, vb_buffer, vec_a, vec_b, counter);
-        vec_mul(va_buffer, vb_buffer, vout_buffer);
-
+    for(int counter = 0; counter < NUM_BLOCK; counter++) {
+        for(int i = 0; i < BLOCK_SIZE; i++) {
+            if (counter * BLOCK_SIZE + i >= VEC_LEN) {
+                va_buffer[i] = 0;
+                vb_buffer[i] = 0;
+            }
+            else {
+                va_buffer[i] = vec_a[counter * BLOCK_SIZE + i];
+                vb_buffer[i] = vec_b[counter * BLOCK_SIZE + i];
+            }
+            vout_buffer[i] = va_buffer[i] * vb_buffer[i];
+        }
+        
+        // compute: for(int i = 0; i < BLOCK_SIZE; i++) {
+        //     // vout_buffer[0] += va_buffer[i] * vb_buffer[i];
+        //     results[0] += va_buffer[i] * vb_buffer[i];
+        // }
 	for(int i = 0; i < BLOCK_SIZE; i++) {
              results[0] += vout_buffer[i];
         }
     }
 }
-
 }
