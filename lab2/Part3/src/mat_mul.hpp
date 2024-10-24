2dd06efc7f9e3d5371b03a4008ef1f122314e529
rathna-siva <rathna.siva02@gmail.com>
2024-10-23 23:01:32 -0500

    something works!

--- lab2/Part3/src/mat_mul.hpp
+++ lab2/Part3/src/mat_mul.hpp
@@ -13,21 +13,21 @@ int8_t a_in[4] = {0};
 int8_t b_in[4] = {0};
 uint64_t cycle = 0;
 
-void mac(int a, int b, int i, int cycle) {
+void mac(int8_t a, int8_t b, int i, int cycle) {
     pe[i] += a*b;
     if(((i+1) % 4) != 0){
         if((cycle%2) == 0){
-            a_out_1[i+1] = a;
+            a_out_1[i] = a;
         } else {
-            a_out_0[i+1] = a;
+            a_out_0[i] = a;
         }
         
     }
     if((i+4) < 16){
         if((cycle%2) == 0){
-            b_out_1[i+4] = b;
+            b_out_1[i] = b;
         } else {
-            b_out_0[i+4] = b;
+            b_out_0[i] = b;
         }
     }
 }
@@ -40,16 +40,16 @@ void mat_mul_mac(int8_t a_buf[4],     // Read-Only Vector 1
     for(int i = 0; i < 16; i++){
         int row = (i/4);
         int col = (i%4);
-        int a_val = 0;
-        int b_val = 0;
+        int8_t a_val = 0;
+        int8_t b_val = 0;
 
         if(row == 0){
             b_val = b_buf[col];
         } else {
             if((cycle%2) == 0){
-                b_val = b_out_0[(row-1)*4 + col];
+                b_val = b_out_0[i-4];
             } else {
-                b_val = b_out_1[(row-1)*4 + col];
+                b_val = b_out_1[i-4];
             }
             
         }
@@ -58,9 +58,9 @@ void mat_mul_mac(int8_t a_buf[4],     // Read-Only Vector 1
             a_val = a_buf[row];
         }  else {
             if((cycle%2) == 0){
-                a_val = a_out_0[(row) + (col-1)];
+                a_val = a_out_0[i-1];
             } else {
-                a_val = a_out_1[(row) + (col-1)];
+                a_val = a_out_1[i-1];
             }
             
         }
--- lab2/Part3/src/matrix_buffer.cpp
+++ lab2/Part3/src/matrix_buffer.cpp
@@ -2,14 +2,15 @@
 #include <stdint.h>
 #include <iostream>
 #include "mat_mul.hpp"
+#include <cstdlib>
 
 // In order for this scheme to work, blocks A and B must share some common dim
 // However, this is fine as in the case of arbitrary matrix multiplication, we
 // must have matrices A and B of dimensions NxM and MxP such that A and B share
 // M as a common dimension
 #define BLOCK_DIM_ROW_A   4
-#define BLOCK_DIM_ROW_B   4 
-#define BLOCK_DIM_COL_A   4
+#define BLOCK_DIM_ROW_B   16 
+#define BLOCK_DIM_COL_A   16
 #define BLOCK_DIM_COL_B   4 
 #define COMMON_DIM        4 
 
@@ -17,20 +18,24 @@ int main() {
   // create some arbitrary matrices
   int8_t mat_a[BLOCK_DIM_ROW_A][BLOCK_DIM_COL_A];
   int8_t mat_b[BLOCK_DIM_ROW_B][BLOCK_DIM_COL_B];
+  int16_t mat_c[BLOCK_DIM_ROW_A][BLOCK_DIM_COL_B];
+  int16_t golden_c[BLOCK_DIM_ROW_A][BLOCK_DIM_COL_B];
 
   std::cout << "Matrix A:" << std::endl;
   for (int8_t i = 0; i < BLOCK_DIM_ROW_A; i++) {
     for (int8_t j = 0; j < BLOCK_DIM_COL_A; j++) {
-      mat_a[i][j] = int8_t(i + j + 1);
-      std::cout << mat_a[i][j] << " ";
+      //mat_a[i][j] = int8_t(i + j + 1);
+      mat_a[i][j] = int8_t(rand());
+      std::cout << int16_t(mat_a[i][j]) << " ";
     } std::cout << std::endl;
   }   std::cout << std::endl;
 
   std::cout << "Matrix B:" << std::endl;
   for (int8_t i = 0; i < BLOCK_DIM_ROW_B; i++) {
     for (int8_t j = 0; j < BLOCK_DIM_COL_B; j++) {
-      mat_b[i][j] = int8_t(i + j + 2);
-      std::cout << mat_b[i][j] << " ";
+      //mat_b[i][j] = int8_t(i + j + 2);
+      mat_b[i][j] = int8_t(rand());
+      std::cout << int16_t(mat_b[i][j]) << " ";
     } std::cout << std::endl;
   }   std::cout << std::endl;
 
@@ -42,9 +47,10 @@ int main() {
   int8_t buf_b[COMMON_DIM] = {0};
 
   // this is where the magic happens
-  for(int i = 0; i < (COMMON_DIM * 2) - 1; i++) {
+  int num_cycles = BLOCK_DIM_COL_A + (BLOCK_DIM_ROW_A - 1) + 3;
+  for(int i = 0; i < num_cycles; i++) {
     for (int j = 0; j < COMMON_DIM; j++) {
-      if (i > (j - 1) && i < (COMMON_DIM + j)) {
+      if (i > (j - 1) && i < (BLOCK_DIM_COL_A + j)) {
         buf_a[j] = mat_a[j][i-j];
         buf_b[j] = mat_b[i-j][j];
       }
@@ -58,17 +64,17 @@ int main() {
     std::cout << "BUFFER A:\t";
     for (int j = 0; j < COMMON_DIM; j++)
       if (i > (j - 1) && i < (COMMON_DIM + j))
-        std::cout << "a[" << j << "][" << i-j << "] = " << buf_a[j] << "\t";
+        std::cout << int16_t(buf_a[j]) << "\t";
       else
-        std::cout << "buf = 0\t";
+        std::cout << "0\t";
     std::cout << std::endl;
 
     std::cout << "BUFFER B:\t";
     for (int j = 0; j < COMMON_DIM; j++)
       if (i > (j - 1) && i < (COMMON_DIM + j))
-        std::cout << "b[" << i-j << "][" << j << "] = " << buf_b[j] << "\t";
+        std::cout << int16_t(buf_b[j]) << "\t";
       else
-        std::cout << "buf = 0\t";
+        std::cout << "0\t";
     std::cout << std::endl;
 
     std::cout << std::endl << std::endl;
@@ -76,6 +82,44 @@ int main() {
     mat_mul_mac(buf_a, buf_b, i);
 
   }
+
+  std::cout << "Matrix A:" << std::endl;
+  for (int8_t i = 0; i < BLOCK_DIM_ROW_A; i++) {
+    for (int8_t j = 0; j < BLOCK_DIM_COL_A; j++) {
+      //mat_a[i][j] = int8_t(i + j + 1);
+      //mat_a[i][j] = int8_t(rand());
+      std::cout << int16_t(mat_a[i][j]) << " ";
+    } std::cout << std::endl;
+  }   std::cout << std::endl;
+
+  std::cout << "Matrix B:" << std::endl;
+  for (int8_t i = 0; i < BLOCK_DIM_ROW_B; i++) {
+    for (int8_t j = 0; j < BLOCK_DIM_COL_B; j++) {
+      //mat_b[i][j] = int8_t(i + j + 2);
+      //mat_b[i][j] = int8_t(rand());
+      std::cout << int16_t(mat_b[i][j]) << " ";
+    } std::cout << std::endl;
+  }   std::cout << std::endl;
+
+  std::cout << std::endl;
+
+  for (int8_t i = 0; i < BLOCK_DIM_ROW_A; i++) {
+    for (int8_t j = 0; j < BLOCK_DIM_COL_B; j++) {
+        golden_c[i][j] = 0;
+    }
+  }
+
+  std::cout << "Golden Matrix C:" << std::endl;
+  for (int8_t i = 0; i < BLOCK_DIM_ROW_A; i++) {
+    for (int8_t j = 0; j < BLOCK_DIM_COL_B; j++) {
+      //mat_b[i][j] = int8_t(i + j + 2);
+      //golden_c[i][j] = 0;
+      for(int8_t k = 0; k < BLOCK_DIM_COL_A; k++){
+        golden_c[i][j] += mat_a[i][k] * mat_b[k][j];
+      }
+      std::cout << int16_t(golden_c[i][j]) << " ";
+    } std::cout << std::endl;
+  }   std::cout << std::endl;
 
   return 0;
 }
